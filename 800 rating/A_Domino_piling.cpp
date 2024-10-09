c4bbdcbadb266db156a795db051bab86c887fbce
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-10-09 20:59:53 +0600

    800 rating problems

---
+++ 800 rating/A_Domino_piling.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/problemset/problem/50/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int m,n; cin >> m>>n;
+    int sum=(m*n)/2;
+    cout<< sum <<endl;
+    return 0;
+}
