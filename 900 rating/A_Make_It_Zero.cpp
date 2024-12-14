b81a869c60ce84407e3bf4fb3fdeacc0ebcaee38
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-14 12:00:00 +0600

    Backdated commit for GitHub streak

--- 900 rating/A_Make_It_Zero.cpp
+++ 900 rating/A_Make_It_Zero.cpp
@@ -1,65 +1,40 @@
-/*
- * Bismillahir Rahmanir Raheem
- *
- * * * * Coder   : abubakaristiak
- * * * * Created : 2025-04-12 || 02:37:24
- * * * * File    : A_Make_It_Zero.cpp
-*/
+#include <iostream>
+#include <vector>
+#define fast() ios::sync_with_stdio(false); cin.tie(0);
 
-
-// https://codeforces.com/contest/1869/problem/A
-
-
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-#define ll long long
-#define pi pair<ll, ll>
-#define asort(v) sort(v.begin(), v.end())
-#define rsort(v) sort(v.begin(), v.end(), greater<>())
-#define yes cout << "YES\n"
-#define no cout << "NO\n"
-#define cyes cout << "Yes\n"
-#define cno cout << "No\n"
-#define endl "\n"
-#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
-using namespace __gnu_pbds;
 using namespace std;
-
-template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
 
 void solve()
 {
-    int n; cin >> n;
+    int n;
+    cin >> n;
     vector<int> v(n);
-    for(int i=0; i<n; i++){
+    for(int i = 0; i < n; i++){
         cin >> v[i];
     }
-    if(n%2==0){
+
+    if(n % 2 == 0){
         cout << 2 << endl;
         cout << 1 << " " << n << endl;
         cout << 1 << " " << n << endl;
-    }else{
+    } else {
         cout << 4 << endl;
-        cout << 1 << " " << n-1 << endl;
-        cout << 1 << " " << n-1 << endl;
-        cout << n-1 << " " << n << endl;
-        cout << n-1 << " " << n << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << 1 << " " << n - 1 << endl;
+        cout << n - 1 << " " << n << endl;
+        cout << n - 1 << " " << n << endl;
     }
 }
-
 
 int main()
 {
     fast();
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--) {
         solve();
     }
 
     return 0;
-
-
-// Alhamdulillah
+    // Alhamdulillah
 }
