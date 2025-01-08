46f08ec5629173cc4436cc87def2d412df3aa8bd
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-01-08 22:26:45 +0600

    added new problem easy tag 800 rating

---
+++ 800 rating/A_Again_Twenty_Five.cpp
@@ -0,0 +1,35 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-01-07 || 23:42:08
+ * * * * File    : A_Again_Twenty_Five.cpp
+ */
+
+
+// https://codeforces.com/problemset/problem/630/A
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    ll n; cin >>  n;
+    cout << 25;
+    return 0;
+
+
+// Alhamdulillah
+}
