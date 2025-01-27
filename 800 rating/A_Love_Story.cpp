641a695c1af87c467a20436564728b89ffb47652
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-01-27 23:38:30 +0600

    added file

---
+++ 800 rating/A_Love_Story.cpp
@@ -0,0 +1,56 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-01-27 || 23:26:57
+ * * * * File    : A_Love_Story.cpp
+ */
+
+
+// https://codeforces.com/problemset/problem/1829/A
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+
+void solve()
+{
+    string s; cin >> s;
+    string contain="codeforces";
+    int cnt=0;
+    for(int i=0; i<s.size(); i++){
+        if(s[i]!=contain[i]){
+            cnt++;
+        }
+        
+    }
+    cout << cnt << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    
+
+    return 0;
+
+
+// Alhamdulillah
+}
