55f9d5ad8f8d27023bac6d6042a003dd08e7c325
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-01-29 23:15:22 +0600

    added new problem easy tag 800 rating

---
+++ 800 rating/A_Special_Permutation.cpp
@@ -0,0 +1,53 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-01-29 || 10:55:58
+ * * * * File    : A_Special_Permutation.cpp
+ */
+
+
+
+// https://codeforces.com/problemset/problem/1454/A
+
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+
+void solve()
+{
+    int n; cin >> n;
+    for(int i=0; i<n; i++){
+        cout << ((i+1) % n) + 1 << " ";
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    
+
+    return 0;
+
+
+// Alhamdulillah
+}
