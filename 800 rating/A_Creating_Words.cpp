ba2098a4de4aefd9b150277f3af621cf9d3c5205
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-01-30 23:42:05 +0600

    added new problem easy tag 800 rating

---
+++ 800 rating/A_Creating_Words.cpp
@@ -0,0 +1,49 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-01-30 || 23:39:41
+ * * * * File    : A_Creating_Words.cpp
+ */
+
+
+// https://codeforces.com/problemset/problem/1985/A
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+
+void solve()
+{
+    string a,b; cin >> a >> b;
+    swap(a[0],b[0]);
+    cout << a << " " << b << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    
+
+    return 0;
+
+
+// Alhamdulillah
+}
