c3550506f2812554a405abc8c84450f4dddb999c
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-03 09:27:36 +0600

    added new problems

---
+++ 900 rating/A_Odd_Divisor.cpp
@@ -0,0 +1,46 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-02-03 || 09:23:24
+ * * * * File    : A_Odd_Divisor.cpp
+ */
+
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+
+void solve()
+{
+    ll n; cin >> n;
+    if((n&(n-1))==0){
+        no;
+    }else yes;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
