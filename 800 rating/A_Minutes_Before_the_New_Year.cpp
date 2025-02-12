5038e9b7ff1ff1bd1a23715173e67aa40ad66b18
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-12 23:46:56 +0600

    added new problem on codeforces

---
+++ 800 rating/A_Minutes_Before_the_New_Year.cpp
@@ -0,0 +1,53 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-02-12 || 23:41:41
+ * * * * File    : A_Minutes_Before_the_New_Year.cpp
+*/
+
+
+// https://codeforces.com/problemset/problem/1283/A
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int h,m; cin >> h >> m;
+    int day=24*60;
+    int time=h*60+m;
+    cout << day-time << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
