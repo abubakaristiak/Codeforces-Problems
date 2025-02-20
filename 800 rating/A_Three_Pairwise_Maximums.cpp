6d9ad60ff7d558717c64cbc85e975159211c827d
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-02-20 23:50:52 +0600

    added new problem

---
+++ 800 rating/A_Three_Pairwise_Maximums.cpp
@@ -0,0 +1,58 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-02-20 || 23:46:53
+ * * * * File    : A_Three_Pairwise_Maximums.cpp
+*/
+
+
+// https://codeforces.com/problemset/problem/1385/A
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll ar[3];
+    cin >> ar[0] >> ar[1] >> ar[2];
+    sort(ar, ar+3);
+    if(ar[1]==ar[2]){
+        yes;
+        cout << ar[2] << " " << ar[0] << " " << ar[0] << endl;
+    }else{
+        no;
+    }
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
