f4987b6741644e96dc627c1780a8b83336f2cf50
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-02 11:51:59 +0600

    removed unnecessary files

--- Sheet 01/A_Say_Hello_With_C.c
+++
@@ -1,9 +0,0 @@
-//https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/A
-#include<stdio.h>
-int main()
-{
-    char c[100];
-    scanf("%s", c);
-    printf("Hello, %s", c);
-    return 0;
-}
