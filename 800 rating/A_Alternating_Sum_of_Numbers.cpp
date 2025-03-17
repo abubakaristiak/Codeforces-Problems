5e621febd573053d0ac3abe6892a62e340d08426
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-03-17 23:45:38 +0600

    added new problem on codeforces 800 rating

---
+++ 800 rating/A_Alternating_Sum_of_Numbers.cpp
@@ -0,0 +1,60 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-03-17 || 23:23:58
+ * * * * File    : A_Alternating_Sum_of_Numbers.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int n; cin >> n; 
+    vector<int> v(n);
+    int odd=0, even=0;
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+    for(int i=0; i<n; i++){
+        if(i%2!=0){
+            odd+=v[i];
+        }else{
+            even+=v[i];
+        }
+    }
+    cout << even-odd << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah---(Ramadan Kareem)
+}
