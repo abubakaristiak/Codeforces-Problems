8199df2741851917ba9eea4ea3af927f9d708f71
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-12 23:55:24 +0600

    added new problem

---
+++ 800 rating/A_One_and_Two.cpp
@@ -0,0 +1,69 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-12 || 23:50:52
+ * * * * File    : A_One_and_Two.cpp
+*/
+
+
+// https://codeforces.com/problemset/problem/1788/A
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int n; cin >> n;
+    vector<int> v(n);
+    for(int i=0; i<n; i++){
+        cin >> v[i];
+    }
+
+    int cnt2=count(v.begin(), v.end(), 2);
+    if(cnt2==0){
+        cout << 1 << endl;
+        return;
+    }
+    int ans=-2, cnt1=0;
+    for(int i=0; i<n; i++){
+        cnt1+=(v[i]==2);
+        if(2*cnt1==cnt2){
+            ans=i;
+            break;;
+        }
+    }
+    cout << ans+1 << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
