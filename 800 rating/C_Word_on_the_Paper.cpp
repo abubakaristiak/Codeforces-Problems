36fe0d2d5f761c6e5aa230e71a6f75f4d07c7728
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-17 23:45:44 +0600

    added new problem

---
+++ 800 rating/C_Word_on_the_Paper.cpp
@@ -0,0 +1,58 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-17 || 23:42:59
+ * * * * File    : C_Word_on_the_Paper.cpp
+*/
+
+// https://codeforces.com/problemset/problem/1850/C
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int n=8; 
+    string res;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            char ch; cin >> ch;
+            if(ch!='.'){
+                res.push_back(ch);
+            }
+        }
+    }
+    cout << res << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
