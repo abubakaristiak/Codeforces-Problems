0a50b0b55d2c977c25e44a80cc279b41b4f5497a
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-23 01:10:38 +0600

    updated this problem

--- 800 rating/A_Make_A_Equal_to_B.cpp
+++ 800 rating/A_Make_A_Equal_to_B.cpp
@@ -1,42 +1,67 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-23 || 01:03:32
+ * * * * File    : A_Make_A_Equal_to_B.cpp
+*/
 
+// https://codeforces.com/contest/1736/problem/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
 using namespace std;
 
-int main() {
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
-    int testCases;
-    cin >> testCases;
 
-    while (testCases--) {
-        int n;
-        cin >> n;
-
-        vector<int> a(n), b(n);
-
-        for (int i = 0; i < n; i++) {
-            cin >> a[i];
+void solve()
+{
+    int n; cin >> n; 
+    vector<int> a(n),b(n);
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+    for(int i=0; i<n; i++){
+        cin >> b[i];
+    }
+    int cntA=0, cntB=0, notCnt=0;
+    for(int i=0; i<n; i++){
+        cntA+=a[i];
+        cntB+=b[i];
+        if(a[i]!=b[i]){
+            notCnt++;
         }
+    }
+    int dif=abs(cntA-cntB);
+    int res=min(notCnt, dif+1);
+    cout << res << endl;
 
-        for (int i = 0; i < n; i++) {
-            cin >> b[i];
-        }
+}
 
-        int sumA = 0, sumB = 0, mismatchCount = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (a[i] != b[i]) {
-                mismatchCount++;
-            }
-            sumA += a[i];
-            sumB += b[i];
-        }
-
-        int result = min(mismatchCount, abs(sumA - sumB) + 1);
-        cout << result << endl;
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
     }
 
     return 0;
+
+
+// Alhamdulillah
 }
