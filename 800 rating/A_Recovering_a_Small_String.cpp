c22ed88e5bd4815a025870fb07f114ba1118436e
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-26 23:40:59 +0600

    added new problem

---
+++ 800 rating/A_Recovering_a_Small_String.cpp
@@ -0,0 +1,58 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-26 || 23:34:35
+ * * * * File    : A_Recovering_a_Small_String.cpp
+*/
+
+// https://codeforces.com/problemset/problem/1931/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    int n; cin >> n;
+    for(int i=1; i<=26; i++){
+        for(int j=1; j<=26; j++){
+            for(int k=1; k<=26; k++){
+                if(i+j+k==n){
+                    cout << char(i+96) << char(j+96) << char(k+96) << endl;
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
