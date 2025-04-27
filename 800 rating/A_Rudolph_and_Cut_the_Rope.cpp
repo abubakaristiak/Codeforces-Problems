a3daca58da52941259753ee5d906a28246a158a5
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-28 00:08:09 +0600

    added new problem

---
+++ 800 rating/A_Rudolph_and_Cut_the_Rope.cpp
@@ -0,0 +1,55 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-27 || 23:48:04
+ * * * * File    : A_Rudolph_and_Cut_the_Rope.cpp
+*/
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    ll n; cin >> n;
+    ll ans=0;
+    while (n--)
+    {
+        ll a,b; cin >> a >> b;
+        ans+=(a>b);
+    }
+    cout << ans << endl;
+    
+}
+
+
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
