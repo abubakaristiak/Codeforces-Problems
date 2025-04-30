0c31ff035dc2159943e44aeec00d348cc33e809e
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-04-30 23:25:31 +0600

    added new problem

---
+++ 800 rating/A_LLPS.cpp
@@ -0,0 +1,56 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-04-30 || 23:21:35
+ * * * * File    : A_LLPS.cpp
+*/
+
+// https://codeforces.com/problemset/problem/202/A
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace __gnu_pbds;
+using namespace std;
+
+template <typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve()
+{
+    string s; cin >> s;
+    char mxCh=*max_element(s.begin(), s.end());
+    string res;
+    for(char ch:s){
+        if(ch==mxCh){
+            res.push_back(ch);
+        }
+    }
+    cout << res << endl;
+}
+
+
+int main()
+{
+    fast();
+    int t=1;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+
+
+// Alhamdulillah
+}
