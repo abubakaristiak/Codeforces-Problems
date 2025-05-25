4128bcfd90a41f1c67bebaf11090e59be598b268
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-05-25 23:07:55 +0600

    added new problem

---
+++ 800 rating/A_Bulbs.cpp
@@ -0,0 +1,79 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-05-25 || 23:04:40
+ * * * * File    : A_Bulbs.cpp
+*//*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+
+// https://codeforces.com/problemset/problem/615/A
+
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp \" \"
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve() {
+    int n,m; cin >> n >> m;
+    set<int> b;
+    afor(i,0,n){
+        int x; cin >> x;
+        afor(j,0,x){
+            int x2; cin >> x2;
+            b.insert(x2);
+        }
+    }
+    if(b.size()==m){
+        yes;;
+    }else no;
+}
+
+
+int main() {
+    fast();
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+    // Alhamdulillah
+}
