3573dff91baccee38c238973c94359fb6db1bc28
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-05-31 22:17:30 +0600

    added new problem

---
+++ 800 rating/B_Serval_and_Inversion_Magic.cpp
@@ -0,0 +1,89 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2025-05-31 || 22:11:26
+ * * * * File    : B_Serval_and_Inversion_Magic.cpp
+*//*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+
+// https://codeforces.com/problemset/problem/1789/B
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    bool ok=false, res=true, cnt=false;
+
+    int i=0, j=s.size()-1;
+    while (i<j)
+    {
+        if(s[i]!=s[j]){
+            if(ok){
+                res=false;
+            }else{
+                cnt=true;
+            }
+        }else if(cnt){
+            ok=true;
+        }
+        i++;
+        j--;
+    }
+    if(res){
+        cyes;
+    }else cno;
+    
+}
+
+
+int main() {
+    fast();
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+    // Alhamdulillah
+}
