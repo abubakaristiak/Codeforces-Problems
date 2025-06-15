ab2dd0803ee17bcaad69c60b5f4d6275f49aee73
Abu Bakar Istiak <abubakar119147@gmail.com>
2025-06-15 18:14:39 +0600

    added new problem

---
+++ 900 rating/B_Three_Threadlets.cpp
@@ -0,0 +1,82 @@
+/*
+||-----------------------------------||
+||        Abu Bakar Istiak           ||
+||     Chattogram Polytechnic        ||
+||        Department of CST          ||
+||    abubakar119147@gmail.com       ||
+||-----------------------------------||
+*/
+
+// https://codeforces.com/problemset/problem/1881/B
+
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define ll long long
+#define ull unsigned long long
+#define pi pair<ll, ll>
+#define vi vector<ll>
+#define vpi vector<pi>
+#define pb push_back
+#define endl "\n"
+#define sp " "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define sz(x) (ll)(x).size()
+#define minus cout << -1 << endl
+#define zero cout << 0 << endl
+#define MAX 100000
+#define MOD 998244353
+#define afor(i,a,b) for (ll i = (a); i < (b); ++i)
+#define rfor(i,a,b) for (ll i = (a); i >= (b); --i)
+#define asort(v) sort((v).begin(), (v).end())
+#define rsort(v) sort((v).begin(), (v).end(), greater<>())
+#define fast() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+template <typename T>
+using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+void solve() {
+    int a,b,c; cin >> a >> b >> c;
+    vector<int> v;
+    v.pb(a);
+    v.pb(b);
+    v.pb(c);
+    int t=4;
+    while (t--)
+    {
+        asort(v);
+        int n=v.size();
+        if(v[0]==v[n-1]){
+            yes;
+            return;
+        }
+        int mx=v[n-1];
+        v.pop_back();
+        v.pb(v[0]);
+        v.pb(mx-v[0]);
+    }
+    no;
+    
+
+}
+
+
+int main() {
+    fast();
+    int t = 1;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+
+    // Alhamdulillah
+}
